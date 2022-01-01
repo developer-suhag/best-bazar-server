6e64787b5dc0516bd3ac2af08d69fa05b9f7f068
Suhag Al Amin <developersuhag@gmail.com>
2022-01-01 17:22:55 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+// const admin = require("firebase-admin");
+require("dotenv").config();
+const ObjectId = require("mongodb").ObjectId;
+// const stripe = require("stripe")(process.env.STRIPE_SECRET);
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+// middle ware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Chil Bazar server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Chil Bazar app listening at ${port}`);
+});
